f7b16520021a85eda6bede3bf5d781cf059d95fb
Ishir Bhan <ishirbhan@gmail.com>
2025-06-09 17:07:10 -0400

    Updated to pane layout

--- app.py
+++ app.py
@@ -8,24 +8,44 @@ from shinywidgets import render_plotly
 # Constants
 
 diastatic_malt_percentage = 0.5
+dough_types = {"lean": "Lean Sourdough", "pan": "Pan Pizza"}
 
 # User Interface
 
-ui.panel_title("Dough Calculator"),
-ui.input_slider("hydration_percentage", "Hydration", 50, 100, 70),
-ui.input_slider("salt_percentage", "Salt", 0, 5, 2),
-ui.input_slider("yeast_percentage", "Yeast", 0, 5, 2),
-ui.input_slider("oil_percentage", "Oil", 0, 10, 0),
-ui.input_slider("sugar_percentage", "Sugar", 0, 20, 0),
-ui.input_numeric("starter", "Starter", 80, min=0, max=500)
-ui.input_slider("wheat_percentage", "Whole Wheat", 0, 50, 0),
-ui.input_checkbox("include_malt",f"Diastatic Malt ({diastatic_malt_percentage}%)", False),
-"Dough Recipe:"
-ui.input_numeric("flour", "Flour Target", 400, min=0, max=1000)
+ui.panel_title("Dough Calculator")
+
+default_hydration = 70
+default_flour_target = 400
+default_salt = 2
+default_yeast = 2
+default_oil = 0
+default_sugar = 0
+default_wheat = 0
+default_starter = 80
+default_malt = False
 
 
-@render.data_frame  
-def text():
+match dough_types:
+    case "lean":
+        default_hydration: 70
+    case "pan":
+        default_hydration: 70
+
+with ui.sidebar(bg="#f8f8f8"):  
+    ui.input_select("dough_type", None, dough_types)
+    ui.input_numeric("flour", "Flour Target", default_flour_target, min=0, max=1000)
+    ui.input_slider("hydration_percentage", "Hydration", 50, 100, default_hydration)
+    ui.input_slider("salt_percentage", "Salt", 0, 5, default_salt)
+    ui.input_slider("yeast_percentage", "Yeast", 0, 5, default_yeast)
+    ui.input_slider("oil_percentage", "Oil", 0, 10, default_oil)
+    ui.input_slider("sugar_percentage", "Sugar", 0, 20, default_sugar)
+    ui.input_numeric("starter", "Starter", default_starter, min=0, max=500)
+    ui.input_slider("wheat_percentage", "Whole Wheat", 0, 50, default_wheat)
+    ui.input_checkbox("include_malt",f"Diastatic Malt ({diastatic_malt_percentage}%)", default_malt)
+
+
+@render.data_frame
+def recipe():
     flour_target = input.flour()
     starter_total = input.starter()
     starter_hydration = 0.5
@@ -36,13 +56,10 @@ def text():
     sugar_fraction = input.sugar_percentage()/100
     wheat_fraction = input.wheat_percentage()/100
 
-
     hydration = input.hydration_percentage()/100
     
     water_target = flour_target * hydration
     water_added = water_target - starter_water
-
-
 
     wheat_flour = flour_target*wheat_fraction
     base_flour = flour_target - starter_flour - wheat_flour
@@ -61,4 +78,3 @@ def text():
     ingredients_dataframe = pd.DataFrame(ingredients.items(), columns=['Ingredient', 'Amount (g)'])
     filtered_ingredients = ingredients_dataframe[ingredients_dataframe["Amount (g)"]!=0]
     return filtered_ingredients
-    
